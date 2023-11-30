4b4ded67f51b77cffd387b1d947731b82355bac9
yato202010 <61349049+Yato202010@users.noreply.github.com>
2023-11-30 14:54:02 +0100

    continue fixing sql

--- backend/src/main/resources/db/init.sql
+++ backend/src/main/resources/db/init.sql
@@ -1,26 +1,58 @@
 -- ---- Database ---- --
 
 -- Main production database
-create database heh
+create database if not exists heh
     default character set utf8;
 
--- ---- Tables and Triggers ---- --
+use heh;
+-- ---- Tables ---- --
 
 -- Table to store the config of the app
 -- INFO : this is used to store the config of the app like the hours at which the orders are not accepted anymore
 -- or the hours at which the orders cannot be canceled anymore
 create table heh.config
 (
-    config_id int auto_increment
+    name  varchar(255) not null
         primary key,
-    name      varchar(255) not null
-        unique,
-    value     varchar(255) not null
+    value varchar(255) not null
 );
 
 -- set the config of the app
 insert into heh.config (name, value)
 values ('cancel_end', '11:00:00');
+
+
+-- Table to store the available categories
+create table heh.categories
+(
+    category_id serial       not null
+        primary key,
+    name        varchar(255) not null
+        unique,
+    description longtext     not null,
+    constraint category_name
+        unique (name)
+)
+    engine = InnoDB
+    default charset = utf8;
+
+-- Table to store available products
+create table heh.products
+(
+    product_id  serial          not null
+        primary key,
+    name        varchar(255)    not null
+        unique,
+    description longtext        not null,
+    price       decimal         not null,
+    stock       int unsigned    not null,
+    image       varchar(255)    not null,
+    category_id bigint unsigned not null,
+    constraint product_fk
+        foreign key (category_id) references heh.categories (category_id)
+)
+    engine = InnoDB
+    default charset = utf8;
 
 -- Table to store the users
 create table heh.users
@@ -44,9 +76,9 @@ create table heh.users
 -- Table to store the cart of each user
 create table heh.carts
 (
-    user_id    int not null,
-    product_id int not null,
-    quantity   int not null default 1,
+    user_id    bigint unsigned not null,
+    product_id bigint unsigned not null,
+    quantity   int unsigned    not null default 1,
     constraint cart_pk
         primary key (user_id, product_id),
     constraint cart_product_fk
@@ -57,38 +89,67 @@ create table heh.carts
     engine = InnoDB
     default charset = utf8;
 
--- Trigger to remove cart when user is deleted it also remove the orders history
-create trigger remove_user
-    before delete
-    on heh.users
-    for each row
-begin
-    delete
-    from heh.carts
-    where heh.carts.user_id = old.user_id;
-    delete
-    from heh.orders
-    where heh.orders.user_id = old.user_id;
-end;
-
--- Table to store available products
-create table heh.products
+-- Table to store the favorites products of each user
+create table heh.favorites
 (
-    product_id  serial       not null
-        primary key,
-    name        varchar(255) not null
-        unique,
-    description longtext     not null,
-    price       decimal      not null,
-    stock       int          not null,
-    image       varchar(255) not null,
-    category_id int          not null
-        unique,
-    constraint product_fk
-        foreign key (category_id) references heh.categories (category_id)
+    user_id    bigint unsigned not null,
+    product_id bigint unsigned not null,
+    constraint favorites_pk
+        primary key (user_id, product_id),
+    constraint favorites_product_fk
+        foreign key (product_id) references heh.products (product_id),
+    constraint favorites_user_fk
+        foreign key (user_id) references heh.users (user_id)
 )
     engine = InnoDB
     default charset = utf8;
+
+-- Table to store the orders
+create table heh.orders
+(
+    order_id     serial          not null
+        primary key,
+    user_id      bigint unsigned not null,
+    order_date   datetime        not null,
+    prepare_date date            not null,
+    -- 0 = canceled, 1 = cancelable, 2 = pending, 3 = ready, 4 = delivered
+    status       int             not null default 1,
+    constraint order_user_fk
+        foreign key (user_id) references heh.users (user_id)
+) engine = InnoDB
+  default charset = utf8;
+
+-- Table to store the products of each order
+-- INFO : this doesn't depend on the product table because we want to keep the products even if they are deleted
+create table heh.orders_products
+(
+    order_id     bigint unsigned not null,
+    product_name varchar(255)    not null
+        unique,
+    description  longtext        not null,
+    quantity     int             not null default 1,
+    price        decimal         not null,
+    constraint order_products_pk
+        primary key (order_id, product_name),
+    constraint order_products_order_fk
+        foreign key (order_id) references heh.orders (order_id)
+)
+    engine = InnoDB
+    default charset = utf8;
+
+-- ---- Triggers ---- --
+
+-- Trigger to remove products when category is deleted
+-- WARN : this remove the category , the associated products and the associated cart
+create trigger remove_category
+    before delete
+    on heh.categories
+    for each row
+begin
+    delete
+    from heh.products
+    where heh.products.category_id = old.category_id;
+end;
 
 -- Trigger to remove product when it's deleted
 create trigger remove_product
@@ -106,62 +167,42 @@ begin
     where heh.favorites.product_id = old.product_id;
 end;
 
--- Table to store the available categories
-create table heh.categories
-(
-    category_id serial       not null
-        primary key,
-    name        varchar(255) not null
-        unique,
-    description longtext     not null,
-    constraint category_name
-        unique (name)
-)
-    engine = InnoDB
-    default charset = utf8;
-
--- Trigger to remove products when category is deleted
--- WARN : this remove the category , the associated products and the associated cart
-create trigger remove_category
+-- Trigger to remove cart when user is deleted it also remove the orders history
+create trigger remove_user
     before delete
-    on heh.categories
+    on heh.users
     for each row
 begin
     delete
-    from heh.products
-    where heh.products.category_id = old.category_id;
+    from heh.carts
+    where heh.carts.user_id = old.user_id;
+    delete
+    from heh.orders
+    where heh.orders.user_id = old.user_id;
 end;
 
--- Table to store the favorites products of each user
-create table heh.favorites
-(
-    user_id    int not null,
-    product_id int not null,
-    constraint favorites_pk
-        primary key (user_id, product_id),
-    constraint favorites_product_fk
-        foreign key (product_id) references heh.products (product_id),
-    constraint favorites_user_fk
-        foreign key (user_id) references heh.users (user_id)
-)
-    engine = InnoDB
-    default charset = utf8;
 
--- Table to store the orders
-create table heh.orders
-(
-    order_id     serial   not null
-        primary key,
-    user_id      int      not null,
-    order_date   datetime not null,
-    prepare_date date     not null,
-    -- 0 = canceled, 1 = cancelable, 2 = pending, 3 = ready, 4 = delivered
-    status       int      not null default 1,
-    constraint order_user_fk
-        foreign key (user_id) references heh.users (user_id)
-) engine = InnoDB
-  default charset = utf8;
+-- Trigger to populate order_product when order is added and empty the cart
+-- INFO : this is triggered when the order is passed
+create trigger populate_orders_products
+    after insert
+    on heh.orders
+    for each row
+begin
+    -- populate order_products from cart
+    insert into heh.orders_products (order_id, product_name, description, price)
+    select new.order_id, heh.products.name, heh.products.description, heh.products.price
+    from heh.carts
+             inner join heh.products on heh.carts.product_id = heh.products.product_id
+    where heh.carts.user_id = new.user_id;
+    -- empty cart
+    delete
+    from heh.carts
+    where heh.carts.user_id = new.user_id;
+end;
 
+/*
+TODO : this need to be fixed
 -- Trigger to set a event that set it as pending when it's cancel_end of the prepare_date and the status is 0
 create trigger set_pending
     before insert
@@ -205,45 +246,7 @@ begin
         alter event @event_name
             on schedule at date_add(new.prepare_date, interval @cancel_end HOUR_MINUTE);
     end if;
-end;
-
--- Table to store the products of each order
--- INFO : this doesn't depend on the product table because we want to keep the products even if they are deleted
-create table heh.orders_products
-(
-    order_id     int          not null,
-    product_name varchar(255) not null
-        unique,
-    description  longtext     not null,
-    quantity     int          not null default 1,
-    price        decimal      not null,
-    constraint order_products_pk
-        primary key (order_id, product_name),
-    constraint order_products_order_fk
-        foreign key (order_id) references heh.orders (order_id)
-)
-    engine = InnoDB
-    default charset = utf8;
-
--- Trigger to populate order_product when order is added and empty the cart
--- INFO : this is triggered when the order is passed
-create trigger populate_orders_products
-    after insert
-    on heh.orders
-    for each row
-begin
-    -- populate order_products from cart
-    insert into heh.orders_products (order_id, product_name, description, price)
-    select new.order_id, heh.products.name, heh.products.description, heh.products.price
-    from heh.carts
-             inner join heh.products on heh.carts.product_id = heh.products.product_id
-    where heh.carts.user_id = new.user_id;
-    -- empty cart
-    delete
-    from heh.carts
-    where heh.carts.user_id = new.user_id;
-end;
-
+end;*/
 
 -- ---- Views ---- --
 
