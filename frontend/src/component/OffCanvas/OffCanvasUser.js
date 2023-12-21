08f967faa550f9ced6665ad0c13dcf4b4c3524d0
MA-Visty <mahalex1@gmail.com>
2023-12-21 21:02:50 +0100

    fix: Login

--- frontend/src/component/OffCanvas/OffCanvasUser.js
+++ frontend/src/component/OffCanvas/OffCanvasUser.js
@@ -1,21 +1,14 @@
 import React, {useRef, useState} from 'react';
 import {Offcanvas, Button, Row, Col, Form, ImputGroup, InputGroup} from 'react-bootstrap';
-import {useDispatchContext} from "../../store/AppContext";
+import {useAppContext, useDispatchContext} from "../../store/AppContext";
 import axios from "axios";
 
 function OffCanvasUser({ show, onHide, type, ...props }) {
-    const [title, setTitle] = useState("Login");
-
-    if (type === "account" ) {
-        setTitle("Account");
-    } else if (type === "register") {
-        setTitle("Register");
-    }
-
+    const { state } = useAppContext();
+    const [title, setTitle] = useState(state.user !== "" ? "Account" : "Login");
     const switchLogRegist = () => {
         setTitle((title) => (title === 'Login' ? 'Register' : 'Login'));
     };
-
     const switchLogAccount = () => {
         setTitle((title) => (title === 'Login' ? 'Account' : 'Login'));
     };
@@ -40,8 +33,6 @@ function UserLogin({switchLogRegist, switchLogAccount}) {
     const [validated, setValidated] = useState(false);
     const email = useRef();
     const password = useRef();
-
-    //const [user, setUser] = useState("");
     const handleLogin = async (event) => {
         event.preventDefault();
         let emailCheck = email.current.value;
@@ -115,42 +106,117 @@ function UserLogin({switchLogRegist, switchLogAccount}) {
 }
 
 function UserRegister({switchLogRegist}) {
+    const { dispatch } = useDispatchContext();
     const [validated, setValidated] = useState(false);
     const email = useRef();
     const password = useRef();
     const confirmpassword = useRef();
+    const [invalid, setInvalid] = useState({})
 
-    const labelFormGroup = [
-        [email, "email", "Email", ""],
-        [password, "password", "Password", ""],
-        [confirmpassword, "password", "Confirm password", "Your password must be 8-20 characters long, contain letters and numbers,\n" +
-        "                                    and must not contain spaces, special characters, or emoji."]
-    ]
+    const handleRegister = async (event) => {
+        event.preventDefault();
+        let emailCheck = email.current.value;
+        let pwd1Check = password.current.value;
+        let pwd2Check = confirmpassword.current.value;
+
+        if(emailCheck === "" || pwd1Check === "" || pwd2Check === "" || pwd1Check !== pwd2Check) {
+            setValidated(true);
+            return;
+        } else {
+            try {
+                const response = await axios
+                    .post("http://localhost:3000/api/auth/register", {
+                        name: "",
+                        username: "",
+                        email: emailCheck,
+                        password: pwd1Check,
+                        passwordConfirmation: pwd2Check,
+                        role: "user",
+                        phone: "",
+                        address: "",
+                        companyName: "",
+                        image: ""
+                    });
+                setValidated(false);
+            } catch (error) {
+                console.error('Registration error:', error);
+                alert('nop');
+                if (error.response && error.response.status === 422) {
+                    // La validation côté serveur a échoué
+                    const errors = error.response.data.errors;
+                    setInvalid({
+                        email: errors && errors.email ? "Invalid email" : null,
+                        pwd1: errors && errors.password ? "Invalid password" : null,
+                        pwd2: errors && errors.passwordConfirmation ? "Invalid password confirmation" : null
+                    });
+                } else {
+                    // Une autre erreur s'est produite (ex: serveur non disponible)
+                    setInvalid({
+                        email: "Registration failed. Please try again later.",
+                        pwd1: null,
+                        pwd2: null
+                    });
+                }
+            }
+            return;
+        }
+    }
 
     return (
         <Form noValidate validated={validated}>
             <Row className="mb-3">
-                {labelFormGroup.map(([ref, type, label, describe]) => (
-                    <Form.Group key={label}>
-                        <Form.Label>{label}</Form.Label>
+                <Form.Group>
+                    <Form.Label>Email</Form.Label>
+                    <InputGroup hasValidation>
+                        <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
                         <Form.Control
                             required
-                            ref={ref}
-                            type={type}
-                            placeholder={`Enter your ${label}`}
+                            ref={email}
+                            type="text"
+                            placeholder="Entrer your email"
                             defaultValue=""
-                            aria-describedby={describe !== "" ? `{$label}HelpBlock` : ""}
+                            isInvalid={!!invalid.email}
                         />
-                        {describe !== "" ?
-                            <Form.Text id={`${label}HelpBlock`} muted>{describe}</Form.Text> : ""}
-                    </Form.Group>
-                ))}
+                        <Form.Control.Feedback type="invalid">
+                            {invalid.email}
+                        </Form.Control.Feedback>
+                    </InputGroup>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Password</Form.Label>
+                    <InputGroup hasValidation>
+                        <InputGroup.Text id="inputGroupPrepend">#</InputGroup.Text>
+                        <Form.Control
+                            required
+                            ref={password}
+                            type="password"
+                            placeholder="Entrer your password"
+                            aria-describedby="passwordHelpBlock"
+                            isInvalid={!!invalid.pwd1}
+                        />
+                    </InputGroup>
+                </Form.Group>
+                <Form.Group>
+                    <InputGroup hasValidation>
+                        <InputGroup.Text id="inputGroupPrepend">#</InputGroup.Text>
+                        <Form.Control
+                            required
+                            ref={confirmpassword}
+                            type="password"
+                            placeholder="Confirm your password"
+                            aria-describedby="passwordHelpBlock"
+                            isInvalid={!!invalid.pwd2}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {invalid.pwd1}
+                            {invalid.pwd2}
+                        </Form.Control.Feedback>
+                    </InputGroup>
+                </Form.Group>
             </Row>
 
             <div className="d-grid gap-2">
-                <Button variant="primary" size="lg">
-                    Confirm
-                </Button>
+                <Button variant="primary" size="lg" onClick={handleRegister}>Confirm</Button>
                 <Button variant="secondary" size="lg" onClick={switchLogRegist}>Cancel</Button>
             </div>
         </Form>
