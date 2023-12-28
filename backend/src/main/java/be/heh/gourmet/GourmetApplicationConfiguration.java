972d58a36de1600c6d61e5af38b5a00e06507e65
yato202010 <61349049+Yato202010@users.noreply.github.com>
2023-12-28 14:31:07 +0100

    feat(category): setup spring configuration

--- backend/src/main/java/be/heh/gourmet/GourmetApplicationConfiguration.java
+++ backend/src/main/java/be/heh/gourmet/GourmetApplicationConfiguration.java
@@ -1,6 +1,8 @@
 package be.heh.gourmet;
 
+import be.heh.gourmet.adapter.out.persistence.CategoryAdapter;
 import be.heh.gourmet.adapter.out.persistence.ProductAdapter;
+import be.heh.gourmet.application.port.in.IManageCategoryUseCase;
 import be.heh.gourmet.application.port.in.IManageProductUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,8 +12,18 @@ import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 @Configuration
 @EnableJdbcRepositories
 public class GourmetApplicationConfiguration {
+    // Repositories
+
+    // Crud only use cases
     @Bean
     public IManageProductUseCase getManageProductUseCase() {
         return new ProductAdapter();
     }
+
+    @Bean
+    IManageCategoryUseCase getManageCategoryUseCase() {
+        return new CategoryAdapter();
+    }
+
+    // Use cases with business logic
 }
