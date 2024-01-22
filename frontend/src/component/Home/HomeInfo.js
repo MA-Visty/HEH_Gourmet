d8d7a9b760ad24038a468444b82404467fa5ed30
MA-Visty <mahalex1@gmail.com>
2024-01-23 00:28:08 +0100

    Update: Home

--- frontend/src/component/Home/HomeInfo.js
+++ frontend/src/component/Home/HomeInfo.js
@@ -1,8 +1,17 @@
 import BulbImage from "../../assets/images/bulb.svg";
+import {Col, Row} from "react-bootstrap";
+import {Link} from "react-router-dom";
 
 function HomeInfo() {
     return (
-        <img style={{objectFit: 'cover', display:"flex", marginLeft:"auto", marginRight:"auto", height:350}} src={BulbImage} alt="error" />
+        <Row style={{marginLeft: "10%", marginTop: "2%", marginRight: "10%"}}>
+            <Col sm={4}>
+                <img style={{objectFit: 'cover', display:"flex", marginLeft:"auto", marginRight:"auto", height:150}} src={BulbImage} alt="error" />
+            </Col>
+            <Col sm={8} className="d-flex align-items-center">
+                <p><span style={{fontWeight: "bold"}}>HEH-Gourmet</span>, une site permettant de <Link to="/Menu">commander</Link> des sandwich gourmet 😊.</p>
+            </Col>
+        </Row>
     );
 }
 
