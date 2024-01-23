570ca5f0bd3465fc23a04c3b606c3cabaa8cfe28
MA-Visty <mahalex1@gmail.com>
2024-01-23 16:33:33 +0100

    feat: ProductDetail

---
+++ frontend/src/component/Product/ProductDetailNewTable.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function ProductDetailNewTable() {
+    return (
+        <></>
+    );
+}
+
+export default ProductDetailNewTable;
---
+++ frontend/src/component/Product/ProductDetailTable.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function ProductDetailTable() {
+    return (
+        <></>
+    );
+}
+
+export default ProductDetailTable;
