4e25f6cd542af6e655e7a29279ea1a785210bc08
yato202010 <61349049+Yato202010@users.noreply.github.com>
2024-01-23 12:01:14 +0100

    fix(docker) : fix env not being passed during deploy

--- backend/src/main/java/be/heh/gourmet/GourmetApplicationConfiguration.java
+++ backend/src/main/java/be/heh/gourmet/GourmetApplicationConfiguration.java
@@ -9,15 +9,38 @@ import be.heh.gourmet.application.domain.service.ManageCartImpl;
 import be.heh.gourmet.application.domain.service.ManagerOrderImpl;
 import be.heh.gourmet.application.domain.service.PaymentImpl;
 import be.heh.gourmet.application.port.in.*;
-import be.heh.gourmet.application.port.out.*;
+import be.heh.gourmet.application.port.out.ICartRepository;
+import be.heh.gourmet.application.port.out.IPaymentClient;
+import be.heh.gourmet.application.port.out.IUserRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 
 @Configuration
 @EnableJdbcRepositories
 public class GourmetApplicationConfiguration {
+    // Data sources
+    @Value("${DB_URL}")
+    private String DB_URL;
+    @Value("${DB_USERNAME}")
+    private String DB_USERNAME;
+    @Value("${DB_PASSWORD}")
+    private String DB_PASSWORD;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource config = new DriverManagerDataSource();
+        config.setUrl(DB_URL);
+        config.setUsername(DB_USERNAME);
+        config.setPassword(DB_PASSWORD);
+        return config;
+    }
+
     // Repositories
     @Bean
     public ICartRepository getCartRepository() {
