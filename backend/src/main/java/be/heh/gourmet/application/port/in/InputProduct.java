aed01ff48d64bc8e208d0b140531871b6dadf800
yato202010 <61349049+Yato202010@users.noreply.github.com>
2024-01-25 09:34:58 +0100

    fix(validation) :fix typo in validation

--- backend/src/main/java/be/heh/gourmet/application/port/in/InputProduct.java
+++ backend/src/main/java/be/heh/gourmet/application/port/in/InputProduct.java
@@ -1,6 +1,7 @@
 package be.heh.gourmet.application.port.in;
 
 import be.heh.gourmet.application.domain.model.Product;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Size;
 import lombok.NonNull;
@@ -11,7 +12,7 @@ public record InputProduct(
         String name,
         @Size(min = 1, max = 2048, message = "description should be between 1 and 2048 characters")
         String description,
-        @Min(value = 0, message = "price should not be less than 0")
+        @DecimalMin(value = "0.1", message = "price should not be less than 0")
         float price,
         @Min(value = 0, message = "stock should not be less than 0")
         int stock,
